e8762fff88b17574a35dd537680ded2b1a20aca5
TuanVo <tuanngocvo01@gmail.com>
2026-04-22 04:20:08 +0000

    add github actions

--- webhook.py
+++ webhook.py
@@ -3,14 +3,9 @@ import subprocess
 
 class Handler(BaseHTTPRequestHandler):
 	def do_POST(self):
-		if self.path=='/deloy':
-			subprocess.call(['bash','/home/tuanvo/myapp/deloy.sh'])
-			self.send_response(200)
-			self.end_headers()
-			self.wfile.write(b'Deloy triggered')
-		else:
-			self.send_reponse(404)
-			self.end_headers()
+		self.send_response(200)
+		self.end_headers()
+		self.wfile.write(b'Webhook is rungning')
 
 server=HTTPServer(('0.0.0.0',5000), Handler)
 print("Listening on port 5000...")
